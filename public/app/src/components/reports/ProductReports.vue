5b5f8b1e6b70c7681c819824f741137e755a9f3d
Aries <malaca.aries29@gmail.com>
2018-04-07 23:40:20 +0800

    a

---
+++ public/app/src/components/reports/ProductReports.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="tab-pane active" id="product-reports-tab" style="position: relative;">
+    </div>
+</template>
+<script>
+    export default {
+        name: 'ProductReports'
+    }
+</script>
